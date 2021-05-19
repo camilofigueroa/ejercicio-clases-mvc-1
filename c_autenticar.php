2564fd810f792aca4e85be5b8eea02b568b8a2a8
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-05-19 09:25:33 -0500

    Agregando sección de autenticar.

---
+++ c_autenticar.php
@@ -0,0 +1,3 @@
+<?php
+
+    include( "v_autenticar.php" );
---
+++ v_autenticar.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title></title>
+
+        <?php include( "estilo.php" ); ?>
+    </head>
+
+    <body>
+
+        <form action="c_autenticar_resultado.php">
+            Documento: <input type="text" name="documento"><br>
+            Clave: <input type="text" name="clave"><br>
+            <input type="sumit" value="Enviar">
+        </form>
+
+    </body>
+</html>
