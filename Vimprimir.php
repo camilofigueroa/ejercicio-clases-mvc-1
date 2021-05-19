b31cdcbd66c4effc074ae6cd5422b473a0018bfd
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-05-19 08:29:42 -0500

    Cambiando el método de imprimir

--- Vimprimir.php
+++ Vimprimir.php
@@ -2,29 +2,33 @@
 
     class Vimprimir
     {
-        static function imprimir( $resultado )
+        static function imprimir( $resultado, $des = null )
         {
-            $salida = "<table border='1px'>";
+            $salida = "";
+
+            if( $des == null ) $salida .= "<table border='1px'>";
 
             while( $fila = mysqli_fetch_array( $resultado ) )
             {
-                $salida .= "<tr>";
+                if( $des == null ) $salida .= "<tr>";
 
                 for( $i = 0; $i < mysqli_num_fields( $resultado ); $i ++ )
                 {
-                    $salida .= "<td>".$fila[ $i ]."</td>";
+                    if( $des == null ) $salida .= "<td>";
+                    $salida .= $fila[ $i ];
+                    if( $des == null ) $salida .= "</td>";
 
                     if( $i + 1 == mysqli_num_fields( $resultado ) )
                     {
-                        $salida .= "<td><a href='#'>Editar</a></td>";
-                        $salida .= "<td><a href='#'>Borrar</a></td>";
+                        if( $des == null ) $salida .= "<td><a href='#'>Editar</a></td>";
+                        if( $des == null ) $salida .= "<td><a href='#'>Borrar</a></td>";
                     }
                 }
 
-                $salida .= "</tr>";
+                if( $des == null ) $salida .= "</tr>";
             }            
 
-            $salida .= "</table>";
+            if( $des == null ) $salida .= "</table>";
 
             return $salida;
         }
